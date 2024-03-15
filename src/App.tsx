66f35d438dd6a022da7f0ee8142fcfa2f97bf05e
santoshi-1 <karuturisantoshi@gmail.com>
2024-03-15 08:51:07 -0400

    creating dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import React, {FC, ReactElement} from 'react';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import React, { FC, ReactElement } from 'react';
+import { customTheme } from './theme/customTheme';
+import { Dashboard } from './pages/dashboard/dashboard';
 
-const App:FC = ():ReactElement => {
-  return <h1>Hello World!</h1>
-}
+const App: FC = (): ReactElement => {
+  return (
+    <ThemeProvider theme={customTheme}>
+      <CssBaseline />
+      <Dashboard />
+    </ThemeProvider>
+  );
+};
 
 export default App;
